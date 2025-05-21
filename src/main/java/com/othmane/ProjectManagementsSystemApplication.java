1bcda523c90d4bf316d4f0e821e2f6fc70920bed
Dbibih Othmane <othmaanedbibih@gmail.com>
2025-05-21 23:19:18 +0100

    first commit

---
+++ src/main/java/com/othmane/ProjectManagementsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.othmane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/othmane/ProjectManagementsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.othmane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
