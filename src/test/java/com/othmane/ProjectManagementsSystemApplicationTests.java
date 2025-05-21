a1ba55dae9d9cc3210a5e87b75c982e9ae806afe
Dbibih Othmane <othmaanedbibih@gmail.com>
2025-05-22 00:50:34 +0100

    Add test

--- src/test/java/com/othmane/ProjectManagementsSystemApplicationTests.java
+++ src/test/java/com/othmane/ProjectManagementsSystemApplicationTests.java
@@ -2,12 +2,18 @@ package com.othmane;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = WebEnvironment.NONE,
+		properties = {
+				"spring.profiles.active=test",
+				"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration,org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+		}
+)
 class ProjectManagementsSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
